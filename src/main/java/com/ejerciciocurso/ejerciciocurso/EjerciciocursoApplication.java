fd5e8fdd9900df8fe23a4424bc98485b9ffba2a9
luismiguelviewnext <135017297+luismiguelviewnext@users.noreply.github.com>
2023-09-26 13:19:21 +0200

    Update EjerciciocursoApplication.java

--- src/main/java/com/ejerciciocurso/ejerciciocurso/EjerciciocursoApplication.java
+++ src/main/java/com/ejerciciocurso/ejerciciocurso/EjerciciocursoApplication.java
@@ -3,9 +3,18 @@ package com.ejerciciocurso.ejerciciocurso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal que sirve como punto de entrada para la aplicación de Spring Boot.
+ * Inicia la aplicación y realiza el escaneo de componentes en los paquetes especificados.
+ */
 @SpringBootApplication (scanBasePackages = {"com.ejerciciocurso.controller ", "com.ejerciciocurso.service"})
 public class EjerciciocursoApplication{
 
+	  /**
+     * Método principal que sirve como punto de entrada para la aplicación.
+     * 
+     * @param args Argumentos de línea de comando.
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(EjerciciocursoApplication.class, args);
 	}
